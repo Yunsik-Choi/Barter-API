fb7c4fb18be178cf1e441098b91d7b3a9c7c062a
Yunsik-Choi <choiys0311@gmail.com>
2022-01-14 16:20:18 +0900

    [feat] User 회원가입 개발 유저 회원가입 개발 issue #1

--- src/main/java/com/project/barter/user/UserController.java
+++ src/main/java/com/project/barter/user/UserController.java
@@ -1,18 +1,24 @@
 package com.project.barter.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.barter.user.dto.UserPost;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class UserController {
 
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+
     @PostMapping("/join")
     public ResponseEntity join(@Validated @RequestBody UserPost userPost){
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(userRepository.save(objectMapper.convertValue(userPost,User.class)));
     }
 
 
