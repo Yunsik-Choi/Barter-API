e0cc12a0ea246ec44d2694183b74b80ab541599e
Yunsik-Choi <choiys0311@gmail.com>
2022-01-15 15:35:07 +0900

    [test] exists userId Jpa test 이미 존재하는 아이디로 회원가입 할 시 에러를 반환하는 테스트 작성 issue #1

--- src/test/java/com/project/barter/user/UserRepositoryTest.java
+++ src/test/java/com/project/barter/user/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -27,6 +28,32 @@ public class UserRepositoryTest {
         User findUser = userRepository.findById(1L).get();
         Assertions.assertThat(1L).isEqualTo(findUser.getId());
         Assertions.assertThat("이름").isEqualTo(findUser.getName());
+    }
+
+    @Test
+    public void alreadyExistsUserId(){
+        User user1 = User.builder()
+                .userId("userId")
+                .password("password")
+                .name("유저1")
+                .birthday(new Birthday(2010,12,12))
+                .email("naver@gmail.com")
+                .phoneNumber("01012345678")
+                .build();
+        User user2 = User.builder()
+                .userId("userId")
+                .password("pass")
+                .name("유저2")
+                .birthday(new Birthday(2020,12,12))
+                .email("google@gmail.com")
+                .phoneNumber("01012345678")
+                .build();
+
+        userRepository.save(user1);
+
+        org.junit.jupiter.api.Assertions.assertThrows(
+                DataIntegrityViolationException.class,
+                ()-> userRepository.save(user2));
     }
 
 
