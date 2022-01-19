820d1461d01f6b93f5314347055e18a56311a1a5
Yunsik-Choi <choiys0311@gmail.com>
2022-01-19 16:19:45 +0900

    [refactor] Refactor User Test 반복되는 UserPost 생성을 UserUtil 클래스 메소드를 사용하도록 수정

--- src/test/java/com/project/barter/user/UserControllerTest.java
+++ src/test/java/com/project/barter/user/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.project.barter.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.barter.user.domain.Birthday;
 import com.project.barter.user.dto.UserPost;
@@ -37,14 +36,7 @@ class UserControllerTest {
     @DisplayName("유저 회원가입 성공")
     @Test
     public void User_Join_Success() throws Exception{
-        UserPost userPost = UserPost.builder()
-                .userId("userId")
-                .password("password")
-                .name("이름")
-                .birthday(new Birthday(2020,12,12))
-                .email("google@gmail.com")
-                .phoneNumber("01012345678")
-                .build();
+        UserPost userPost = UserUtils.getCompleteUserPost();
 
         User user = objectMapper.convertValue(userPost,User.class);
         user.setId(1L);
@@ -82,15 +74,7 @@ class UserControllerTest {
 
     @Test
     public void alreadyExistsUserIdJoin() throws Exception{
-        UserPost userPost = UserPost.builder()
-                .userId("userId")
-                .password("password")
-                .name("이름")
-                .birthday(new Birthday(2020,12,12))
-                .email("google@gmail.com")
-                .phoneNumber("01012345678")
-                .build();
-
+        UserPost userPost = UserUtils.getCompleteUserPost();
         User user = objectMapper.convertValue(userPost,User.class);
         user.setId(1L);
         when(userRepository.findUserByUserId(userPost.getUserId())).thenReturn(Optional.of(user));
@@ -130,7 +114,7 @@ class UserControllerTest {
                 .content(objectMapper.writeValueAsString(userPost)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andDo(document("User 회원가입 실패 Exists UserId",
+                .andDo(document("User Post Binding 실패",
                         requestFields(
                                 fieldWithPath("userId").description("유저 로그인 아이디"),
                                 fieldWithPath("password").description("유저 로그인 비밀번호"),
---
+++ src/test/java/com/project/barter/user/UserUtils.java
@@ -0,0 +1,20 @@
+package com.project.barter.user;
+
+import com.project.barter.user.domain.Birthday;
+import com.project.barter.user.dto.UserPost;
+
+public class UserUtils {
+
+    public static UserPost getCompleteUserPost(){
+        return UserPost.builder()
+                .userId("userId")
+                .password("password")
+                .name("이름")
+                .birthday(new Birthday(2020,12,12))
+                .email("google@gmail.com")
+                .phoneNumber("01012345678")
+                .build();
+    }
+
+
+}
